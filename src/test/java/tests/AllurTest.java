955705be63171c8310a152a3c5422590f1c2a092
Alexandr Donskov <mrad@list.ru>
2022-05-03 12:42:39 +0300

    added beforeAll

--- src/test/java/tests/AllurTest.java
+++ src/test/java/tests/AllurTest.java
@@ -1,8 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
@@ -11,6 +17,19 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class AllurTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://www.ozon.ru/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = "chrome";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
 
     @Test
     @DisplayName("Проверка Ozon в хедере")
@@ -22,6 +41,8 @@ public class AllurTest {
         step("Проверить наличие надписи Ozon в хедере", () -> {
             $("[data-widget=header]").should(text("Ozon"));
         });
-
     }
+
+//
+
 }
